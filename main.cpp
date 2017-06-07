5381f2b4f84eaa76524a68a37aa06250ad121e91
Епишкин Антон <epishunt@inbox.ru>
2017-06-08 01:27:44 +0300

    умножение заменено на деление

--- main.cpp
+++ main.cpp
@@ -8,5 +8,8 @@ int main()
     cout << "Enter A and B: ";
     cin  >> a >> b;
     cout << "A + B = " << a + b << '\n';
-    cout << a*2;
+    cout << "A + B = " << a + b << '\n'
+         << "A * 2 = " << a * 2 << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A / B = " << a / b << '\n';
 }
